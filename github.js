ad0358211bf4d9b64a3b00f4fb9275187b487076
kuldipcode <kuldip.code@gmail.com>
2016-05-19 17:57:50 -0700

    github.js update

--- github.js
+++ github.js
@@ -1,46 +1,61 @@
 var myAAp = angular.module("sampleAAp",[]);
 
-myAAp.controller("myCtrl2",function($scope,$http){
+// Controller
+myAAp.controller("myCtrl2",function($scope,httpService,repoService){
 
-
+// User click event 	
 $scope.showInfo = function(){
-	var userName = $scope.uName;
-	
-	$http({
-		url:'https://api.github.com/users/'+userName+'',
-		method:"GET",
-		dataType : 'json'	
-	})
-	.success(function(resp){
-		console.log(resp);
+	httpService.getUsernameInfo($scope.uName).success(function(resp){
 		 $scope.id = resp.id;	
 		 $scope.login = resp.login;		
 		 $scope.name = resp.name;		
 		 $scope.company = resp.company;
 		 $scope.location = resp.location;
-		 $scope.email = resp.email;			
+		 $scope.email = resp.email;					
 	})
 	.error(function(resp){
 		console.log(resp);
-	});
+	});	
 
-	$http({
-		url:'https://api.github.com/users/'+userName+'/repos',
-		method:"GET",
-		dataType : 'json'	
-	})
-	.success(function(resp){
-		console.log(resp);
+	repoService.getReponameInfo($scope.uName).success(function(resp){
 		$scope.rId = resp[0].id;	
 		$scope.rName = resp[0].name;
 		$scope.rFull = resp[0].full_name;		
 		$scope.rOlogin = resp[0].owner.login;	
 		$scope.rOid = resp[0].owner.id;
-		$scope.rOurl = resp[0].owner.url;
+		$scope.rOurl = resp[0].owner.url;				
 	})
 	.error(function(resp){
 		console.log(resp);
-	});
-};
-$scope.showInfo();
+	});		
+   };
+});
+
+// Factory for Username Info 
+myAAp.factory('httpService', function($http, $log){
+	return {
+  	getUsernameInfo:function(uName, callback){
+  		
+  		var request = $http.get('https://api.github.com/users/'+uName+'',{
+		method:"GET",
+		dataType : 'json'	
+	})
+  		return request;	
+   }
+  };             
+});
+
+// Factory for Repo Info
+
+myAAp.factory('repoService', function($http, $log){
+
+  return {
+  	getReponameInfo:function(uName, callback){
+  		var request = $http.get('https://api.github.com/users/'+uName+'/repos',{
+		method:"GET",
+		dataType : 'json'	
+	})
+  		return request;	
+   }
+  };             
 });
